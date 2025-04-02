9af52fb77609af449376705b512bee0788ecf99f
Eric Martins <ericmartis123@gmail.com>
2025-04-02 16:47:02 -0300

    correção do botão menu versão mobile | adição de novo banner

--- scriptWhatApp.js
+++ scriptWhatApp.js
@@ -1,11 +1,18 @@
 function acao(event) {
-    const nome = document.getElementById("nome").value;
-    const menssagem = document.getElementById("menssagemss").value;
-    const fone = "5563992077518";
+  const nome = document.getElementById("nome").value;
+  const menssagem = document.getElementById("menssagemss").value;
+  const fone = "5563992077518";
 
-    const texto = `Olá! Me chamo ${nome}, ${menssagem}`;
-    const msgFormatada = encodeURIComponent(texto);
+  const texto = `Olá! Me chamo ${nome}, ${menssagem}`;
+  const msgFormatada = encodeURIComponent(texto);
 
-    const url = `https://wa.me/${fone}/?text=${msgFormatada}`;
-    window.open(url, "_blamk");
-  }
+  const url = `https://wa.me/${fone}/?text=${msgFormatada}`;
+  window.open(url, "_blamk");
+}
+
+// Adiciona funcionalidade ao botão hamburguer
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  document.querySelector(".nav-menu").classList.toggle("active");
+  this.querySelector("i").classList.toggle("fa-times");
+  this.querySelector("i").classList.toggle("fa-bars");
+});
